6538d731e37f1c1a934bb821d234f96e081f7e19
lyne cha <lynecha@berkeley.edu>
2022-03-09 15:55:37 -0800

    finished up to step 5 through 6

--- connect4.js
+++ connect4.js
@@ -19,15 +19,14 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard(width, height) {
   for (let i = 0; i < height; i++) {
-    board[i] = new Array(width);
+    board[i] = new Array(width).fill();
   }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-  let htmlBoard = document.getElementById("board");
+  const htmlBoard = document.getElementById("board");
 
   // Creating a top row w/ id: column-top with an event listener that handleClick 
   let top = document.createElement("tr");
@@ -48,19 +47,12 @@ function makeHtmlBoard() {
   // uses HEIGHT to create table rows
   // uses WIDTH to create table cells for each row
   for (let y = 0; y < HEIGHT; y++) {
-    // TODO: Create a table row element and assign to a "row" variable
     let tableRow = document.createElement("tr");
     for (let x = 0; x < WIDTH; x++) {
-      // TODO: Create a table cell element and assign to a "cell" variable
       let tableCell = document.createElement("td");
-      tableCell.setAttribute("id", y + "-" + x)
-      // TODO: add an id, y-x, to the above table cell element
-      // you'll use this later, so make sure you use y-x
-
-      // TODO: append the table cell to the table row
+      tableCell.setAttribute("id", y + "-" + x);
       tableRow.append(tableCell);
     }
-    // TODO: append the row to the html board
     htmlBoard.append(tableRow);
   }
 }
@@ -75,7 +67,14 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  let circle = document.createElement("div");
+  circle.classList.add("piece");
+  if (currPlayer === 1) {
+    circle.classList.add("p1");
+  }
+  else {
+    circle.classList.add("p2");
+  }
 }
 
 /** endGame: announce game end */
@@ -98,6 +97,8 @@ function handleClick(evt) {
 
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
+
+  board[y][x] = currPlayer;
   placeInTable(y, x);
 
   // check for win
@@ -107,9 +108,15 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  if (board.every(row => row.every(cell => cell !== undefined))) {
+    endGame("it's a tie");
+  }
+  
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  let currPlayer = (currPlayer == 2) ? 1 : 2;
+
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -151,5 +158,5 @@ function checkForWin() {
   }
 }
 
-makeBoard();
+makeBoard(WIDTH,HEIGHT);
 makeHtmlBoard();
